d61559e13b85e3cbedee3eab136c13ee1ac64247
Ayzah Gardezi <116636136+AyzahGardezi@users.noreply.github.com>
2024-06-06 13:49:07 +0500

    Added Sound

--- Ayzah/GD50/Pong/main.lua
+++ Ayzah/GD50/Pong/main.lua
@@ -27,6 +27,12 @@ function love.load()
 
     love.graphics.setFont(smallFont)
 
+    sounds = {
+        ['paddle_hit'] = love.audio.newSource('sounds/paddle_hit.wav', 'static'),
+        ['score'] = love.audio.newSource('sounds/score.wav', 'static'),
+        ['wall_hit'] = love.audio.newSource('sounds/wall_hit.wav', 'static')
+    }
+
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
     fullscreen = false,
     resizable = false,
@@ -66,6 +72,7 @@ function love.update(dt)
             else
                 ball.dy = 200
             end
+            sounds['paddle_hit']:play()
         end
 
         if ball:collides(player2) then
@@ -77,19 +84,23 @@ function love.update(dt)
             else
                 ball.dy = 200
             end
+            sounds['paddle_hit']:play()
         end
 
         if ball.y <= 0 then
             ball.y = 0
             ball.dy = -ball.dy
+            sounds['wall_hit']:play()
         end
 
         if ball.y >= VIRTUAL_HEIGHT - ball.height then
             ball.y = VIRTUAL_HEIGHT - ball.height
             ball.dy = -ball.dy
+            sounds['wall_hit']:play()
         end
 
         if ball.x < 0 then
+            sounds['score']:play()
             servingPlayer = 1
             player2Score = player2Score + 1
             
@@ -103,6 +114,7 @@ function love.update(dt)
         end
 
         if ball.x > VIRTUAL_WIDTH then 
+            sounds['score']:play()
             servingPlayer = 2
             player1Score = player1Score + 1
 
