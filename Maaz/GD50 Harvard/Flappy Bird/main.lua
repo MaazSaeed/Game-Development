5295d2683156e5cd821aef94934a17d8684c0473
Maaz Saeed <116943667+MaazSaeed@users.noreply.github.com>
2024-06-09 21:37:16 +0500

    GD50 A1 - Procedural Pipes Generation

--- Maaz/GD50 Harvard/Flappy Bird/main.lua
+++ Maaz/GD50 Harvard/Flappy Bird/main.lua
@@ -3,6 +3,8 @@ push = require 'push'
 Class = require 'class'
 
 require 'Bird'
+require 'Pipe'
+
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
@@ -22,6 +24,10 @@ local GROUND_SCROLL_SPEED = 60
 local BACKGROUND_LOOPING_POINT = 413
 
 local bird = Bird()
+local pipes = {}
+
+
+local spawnTimer = 0
 
 
 function love.load()
@@ -29,6 +35,7 @@ function love.load()
     
     love.window.setTitle('Flappy Bird')
 
+    math.randomseed(os.time())
 
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         vsync = true,
@@ -67,8 +74,30 @@ function love.update(dt)
     groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) 
     % VIRTUAL_WIDTH
 
+    spawnTimer = spawnTimer + dt
+
+    if spawnTimer > 2 then
+        table.insert(pipes, Pipe())
+        spawnTimer = 0 -- resetting  to wait for 2 more seconds before adding a new pipe
+    end
+
     bird:update(dt)
+
+    for k, pipe in pairs(pipes) do
+        pipe:update(dt)
+        
+        -- when this pipe is off the left edge of the screen delete it
+        if pipe.x < -pipe.width then
+            table.remove(pipes, k)
+        end  
+
+    end
+
+
+    -- reset input table
     love.keyboard.keysPressed = {}
+
+
 
 end
 
@@ -76,11 +105,17 @@ end
 
 function love.draw()
     push:start()
+
     love.graphics.draw(background, -backgroundScroll, 0)
     
+    for k, pipe in pairs(pipes) do
+        pipe:render()
+    end
+
     love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
 
     bird:render()
+
     push:finish()
 end
 
