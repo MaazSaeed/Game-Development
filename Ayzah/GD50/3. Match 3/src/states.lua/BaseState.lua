2bb94151ec2f15a0b93f40db6b648f3cc9d7740d
Ayzah Gardezi <116636136+AyzahGardezi@users.noreply.github.com>
2024-06-28 20:43:37 +0500

    Update BaseState.lua

---
+++ Ayzah/GD50/3. Match 3/src/states.lua/BaseState.lua
@@ -0,0 +1,7 @@
+BaseState = Class{}
+
+function BaseState:init() end
+function BaseState:enter(def) end
+function BaseState:update(dt) end
+function BaseState:render() end
+function BaseState:exit() end
