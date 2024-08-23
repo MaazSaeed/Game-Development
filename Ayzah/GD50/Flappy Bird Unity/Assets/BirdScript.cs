8b3b16bf11ab6fbb5fbccad04501f296cc2ce2bb
Ayzah Gardezi <116636136+AyzahGardezi@users.noreply.github.com>
2024-08-23 10:38:06 +0500

    Flappy Bird in Unity

---
+++ Ayzah/GD50/Flappy Bird Unity/Assets/BirdScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    public Rigidbody2D myRigidbody; // can access this from outside the script as it is public
+    public float flapStrength;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) && birdIsAlive)
+        {
+            myRigidbody.velocity = Vector2.up * flapStrength;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
+    }
+}
---
+++ Ayzah/GD50/Flappy Bird Unity/Assets/LogicScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LogicScript : MonoBehaviour
+{
+    public int playerScore = 0;
+    public Text scoreText;
+    public GameObject GameOverScreen;
+
+    //[ContextMenu("Increase Score")]
+    public void addScore(int incrementalValue)
+    {
+        playerScore += incrementalValue;
+        scoreText.text = playerScore.ToString();
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver()
+    {
+        GameOverScreen.SetActive(true);
+    }
+}
